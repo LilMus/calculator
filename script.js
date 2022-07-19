b3fb5f0d389aab1d7adee10ab9c9bd96fbc4b996
Mustapha <mustapha.seghir@gmail.com>
2022-07-19 19:01:55 +0200

    Add some event listener
    Event listeners for numbers and operators button
    The calculator is now able to display the digits and operators in the display and calculation area

--- script.js
+++ script.js
@@ -9,6 +9,11 @@ const suppress = document.querySelector(".div3");
 const comma = document.querySelector(".div19");
 const plusMinus = document.querySelector(".div17");
 
+let firstNumber = 0;
+let secondNumber = 1;
+let operatorClicked = false;
+let isReset = false;
+
 const add = (a, b) => a + b;
 const minus = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -18,3 +23,61 @@ const divide = (a, b) => {
 	}
 	return a / b;
 };
+
+const resetDisplayArea = () => (displayArea.textContent = "");
+
+const addToDisplayArea = content => {
+	displayArea.textContent += content;
+};
+
+const addToCalculationArea = content => {
+	calculationArea.textContent += content;
+};
+
+const addNumberEventListener = () => {
+	for (let number of numbers) {
+		number.addEventListener("click", event => {
+			addToDisplayArea(event.target.textContent);
+		});
+	}
+};
+
+const addOperatorToDisplay = operator => {
+	if (operatorClicked) {
+		calculationArea.textContent = calculationArea.textContent.slice(0, -3);
+		if (operator === operators[0]) {
+			addToCalculationArea(" / ");
+		} else if (operator === operators[1]) {
+			addToCalculationArea(" x ");
+		} else if (operator === operators[2]) {
+			addToCalculationArea(" - ");
+		} else if (operator === operators[3]) {
+			addToCalculationArea(" + ");
+		}
+	} else {
+		firstNumber = parseInt(displayArea.textContent);
+		addToCalculationArea(firstNumber);
+		if (operator === operators[0]) {
+			addToCalculationArea(" / ");
+		} else if (operator === operators[1]) {
+			addToCalculationArea(" x ");
+		} else if (operator === operators[2]) {
+			addToCalculationArea(" - ");
+		} else if (operator === operators[3]) {
+			addToCalculationArea(" + ");
+		}
+		operatorClicked = true;
+		resetDisplayArea();
+	}
+};
+
+const addOperatorEventListener = () => {
+	for (let operator of operators) {
+		operator.addEventListener("click", event => {
+			addOperatorToDisplay(operator);
+		});
+	}
+};
+
+addNumberEventListener();
+addOperatorEventListener();
